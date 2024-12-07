096b87a4faadd51184fac9105d63f37dee6d8cc1
Ian Macalinao <github@igm.pub>
2024-12-07 17:01:59 +0800

    configs index file

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,12 +1,1 @@
-import type { TSESLint } from "@typescript-eslint/utils";
-
-import { default as base } from "./configs/base.js";
-import { default as react } from "./configs/react.js";
-
-export const configs: {
-  base: TSESLint.FlatConfig.ConfigArray;
-  react: TSESLint.FlatConfig.ConfigArray;
-} = {
-  base,
-  react,
-};
+export * as configs from "./configs/index.js";
