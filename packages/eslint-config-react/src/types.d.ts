a9398da735a8a91c1e91e5cef163ada330dceaa9
Ian Macalinao <github@igm.pub>
2025-06-14 00:01:02 +0800

    fix types.d.ts format

--- packages/eslint-config-react/src/types.d.ts
+++ packages/eslint-config-react/src/types.d.ts
@@ -1,6 +1,6 @@
-declare module "eslint-plugin-react-hooks" {
-  import type { TSESLint } from "@typescript-eslint/utils";
+import type { TSESLint } from "@typescript-eslint/utils";
 
+declare module "eslint-plugin-react-hooks" {
   const plugin: TSESLint.FlatConfig.Plugin & {
     configs: {
       recommended: TSESLint.FlatConfig.Config;
